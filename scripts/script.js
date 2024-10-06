4347fae6864b8ecac071e99231f1eb6eee5582d4
Kafka-dev-139 <chanceslucky003@gmail.com>
2024-10-06 08:07:09 +0300

    - setting up a raw home page with dummy links - aside menu appears and dissapears on click - basic design

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+/* makes sidebar appear or dissapear on click */
+function myFunction() {
+  var x = document.getElementById("MySidebar");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+/* Animation effect */
